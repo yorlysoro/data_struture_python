66bc6411e08e5af0fdc8ff96f2ee1c68d0497206
yorlysoro <yorlysoro@gmail.com>
2022-08-04 11:06:28 -0400

    linked, circular list nodes

--- cube_run.py
+++ cube_run.py
@@ -1,16 +1,17 @@
 from cube import Cube
 
+
 def cubeReto():
-    cubo = Cube(3,3,3)
-    
+    cubo = Cube(3, 3, 3)
+
     for row in range(cubo.get_height()):
         for column in range(cubo.get_width()):
             for depth in range(cubo.get_depth()):
                 cubo[row][column][depth] = f'[file {row}, columna {column}, casilla {depth}]'
-    
+
     print(cubo)
-    
-    
+
+
 def run():
     cubeReto()
 
